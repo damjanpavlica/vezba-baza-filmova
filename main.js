ad67dcf692b14786b9bb16e140732e5b1391e673
Damjan Pavlica (materijali) <damjan.pavlica@code.edu.rs>
2019-03-11 19:59:16 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const prikaz = document.getElementById('prikaz')
 const kriterij = document.getElementById('kriterij')
 
+let sviFilmovi = []
+let rezultati = []
+
+function render(niz) {
+  let sablon = ''
+  for (var i = 0; i < niz.length; i++) {
+    sablon += `<h3>${niz[i].naziv}</h3>`
+  }
+  prikaz.innerHTML = sablon
+}
+
 fetch('https://baza-filmova.herokuapp.com/filmovi/ ')
   .then(res => res.json())
   .then(data => {
-    console.log(data)
-    // TODO: renderovati prikaz
+    sviFilmovi = rezultati = data
+    render(rezultati)
   })
 
 kriterij.addEventListener('input', function() {
-  console.log(kriterij.value);
-  // TODO: renderovati nov prikaz
+  rezultati = sviFilmovi.filter(film => film.naziv.includes(kriterij.value))
+  render(rezultati)
 })
